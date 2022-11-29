898c820d9501f333dd38e18f3e0e433197a671bc
Siv3rn <anggito732@gmail.com>
2022-11-29 08:41:41 +0700

    idk

---
+++ Sqlite/app/src/main/java/com/example/sqlite/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.sqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+}
