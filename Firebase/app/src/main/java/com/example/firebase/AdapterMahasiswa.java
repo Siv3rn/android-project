54aa9ad29bbe82befd2f901835d563cbfbca6202
Siv3rn <anggito732@gmail.com>
2022-11-29 15:02:52 +0700

    delete

--- Firebase/app/src/main/java/com/example/firebase/AdapterMahasiswa.java
+++ Firebase/app/src/main/java/com/example/firebase/AdapterMahasiswa.java
@@ -1,9 +1,12 @@
 package com.example.firebase;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -40,6 +43,23 @@ public class AdapterMahasiswa extends RecyclerView.Adapter<AdapterMahasiswa.myVi
             holder.tv_nama.setText("nama :" + data.getNama());
             holder.tv_matkul.setText("nama :" + data.getMatkul());
             holder.tv_alamat.setText("nama :" + data.getAlamat());
+            holder.btn_hapus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+                    builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    }).setMessage("Are you sure to delete this data");
+                }
+            });
 
 
     }
@@ -50,6 +70,7 @@ public class AdapterMahasiswa extends RecyclerView.Adapter<AdapterMahasiswa.myVi
     }
     public class myViewHolder extends RecyclerView.ViewHolder {
         TextView tv_nama,tv_matkul,tv_alamat;
+        ImageView btn_hapus;
         CardView cardView;
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -57,6 +78,7 @@ public class AdapterMahasiswa extends RecyclerView.Adapter<AdapterMahasiswa.myVi
             tv_matkul = itemView.findViewById(R.id.tv_maktul);
             tv_alamat = itemView.findViewById(R.id.tv_alamat);
             cardView = itemView.findViewById(R.id.card_hasil);
+            btn_hapus = itemView.findViewById(R.id.btn_hapus);
         }
     }
 
